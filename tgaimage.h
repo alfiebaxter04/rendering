426fce782ee722d83cc27bc649a296990d050f13
Alfie Baxter <alfie.baxter04@gmail.com>
2026-03-15 19:39:04 +0000

    TGA class

---
+++ tgaimage.h
@@ -0,0 +1,102 @@
+#ifndef TGA_IMAGE_H
+#define TGA_IMAGE_H
+
+#include <fstream>
+#include <cstdint>
+#include <cstring>
+#include <string>
+
+namespace TGA {
+
+    struct TGAColor {
+        int b, g, r, a;
+        constexpr TGAColor(int b, int g, int r, int a)
+            : b(b), g(g), r(r), a(a) {};
+    };
+
+    #pragma pack(push, 1)
+    struct TGAHeader {
+        char  id_length            = 0;
+        char  color_map_type       = 0;
+        char  image_type           = 2;
+        short first_entry_index    = 0;
+        short color_map_length     = 0;
+        char  color_map_entry_size = 0;
+        short x_origin             = 0;
+        short y_origin             = 0;
+        short width;
+        short height;
+        char  pixel_depth;
+        char  image_descriptor     = 0;
+    };
+    #pragma pack(pop)
+
+    class TGAImage {
+    public:
+        enum TGAPixelFormat {
+            GRAYSCALE = 1,
+            RGB = 3,
+            RGBA = 4
+        };
+
+        TGAImage(int width, int height, TGAPixelFormat format)
+            : width(width), height(height), format(format)
+        {
+            bytes_per_pixel = static_cast<int>(format);
+            total_size = width * height * bytes_per_pixel;
+            data = new uint8_t[total_size]();
+            std::memset(data, 0, total_size);
+
+            header.width = width;
+            header.height = height;
+            header.pixel_depth = bytes_per_pixel * 8;
+        }
+
+        ~TGAImage() {
+            delete[] data;
+        }
+
+        void set(int x, int y, TGAColor color) {
+            if (x < 0 || y < 0 || x >= width || y >= height) return;
+
+            int index = (x + y * width) * bytes_per_pixel;
+
+            switch (format) {
+                case TGAPixelFormat::GRAYSCALE:
+                    data[index] = color.b;
+                    break;
+                case TGAPixelFormat::RGB:
+                    data[index]   = color.b;
+                    data[index+1] = color.g;
+                    data[index+2] = color.r;
+                    break;
+                case TGAPixelFormat::RGBA:
+                    data[index]   = color.b;
+                    data[index+1] = color.g;
+                    data[index+2] = color.r;
+                    data[index+3] = color.a;
+                    break;
+            }
+        }
+
+        void write_tga_file(const std::string& filename) {
+            std::ofstream out(filename, std::ios::binary);
+            if (!out) return;
+
+            out.write(reinterpret_cast<char*>(&header), sizeof(TGAHeader));
+            out.write(reinterpret_cast<char*>(data), total_size);
+
+            out.close();
+        }
+
+    private:
+        int width, height;
+        TGAHeader header;
+        uint8_t* data;
+        TGAPixelFormat format;
+        int bytes_per_pixel;
+        int total_size;
+    };
+}
+
+#endif
